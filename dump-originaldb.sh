751cf49c9d1bf43cc9d64417dd74bb0285203a9c
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2015-11-22 10:57:52 +0100

    Add a script that dumps the original SQL database

---
+++ dump-originaldb.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# remarque: ce script doit être lancé sur le système encore fonctionnel. Si on récupère uniquement la partition racine, 
+# il suffit de s'y rendre, puis d'utiliser la commande chroot pour simuler l'ancien système, le temps de récupérer 
+# la base de données
+echo "Démarrage de MySQL"
+/etc/init.d/mysql start
+
+sleep 5
+echo "Mise à jour de la base"
+SQL="SELECT t_bande.ban_nom, tit_file, tit_nom, alb_nom, t_groupe.grp_nom, mus_nom, t_titre.tit_code 
+    INTO OUTFILE '/tmp/backup-titres.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' 
+    FROM 
+        t_titre
+    LEFT JOIN t_musical ON t_titre.tit_code=t_musical.tit_code
+    LEFT JOIN t_bande ON t_titre.ban_code=t_bande.ban_code
+    LEFT JOIN t_type_musical ON t_musical.mus_code=t_type_musical.mus_code
+    LEFT JOIN t_album ON t_album.alb_code = t_titre.alb_code
+    LEFT JOIN t_groupe ON t_groupe.grp_code=t_titre.grp_code"
+
+mysql paulo3 -e "$SQL;"
+
+echo "Arrêt de MySQL"
+/etc/init.d/mysql stop
