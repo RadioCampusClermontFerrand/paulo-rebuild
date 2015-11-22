4a25e64bf762f9bf00410519aaeacbc8c2b878fd
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2015-11-22 12:08:19 +0100

    Add a supplementary mount point

--- mount-paulo.sh
+++ mount-paulo.sh
@@ -3,3 +3,4 @@
 FICHIER=/media/jm/Elements/backup/xen/paulo
 sudo mount -o loop,offset=32256 $FICHIER /mnt/paulo/
 sudo mount -o loop,offset=3159359488 $FICHIER /mnt/paulo/home/
+sudo mount -o loop /media/jm/Elements/backup/xen/paulo-home /mnt/paulo-old-home/
